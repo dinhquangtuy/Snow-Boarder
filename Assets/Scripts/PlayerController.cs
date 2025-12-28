0343686f1655732a482f5759379ffccd1b4992eb
dinhquangtuy <dinhquangtuy.it@gmail.com>
2025-12-28 13:40:24 +0700

    Update Sufface Collider for character spinning

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private Rigidbody2D _rb2d;
+    [SerializeField] private float torqueAmount;
+    void Start() {
+        _rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        float moveInput = Input.GetAxisRaw("Horizontal");
+        Debug.Log(moveInput);
+        _rb2d.AddTorque(-moveInput * torqueAmount);
+    }
+}
